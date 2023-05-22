e14ceabeb4c33b7d088f68e00820d9aa0060c3cd
ieyildiz <yildiz.ie58@gmail.com>
2023-05-22 20:53:49 +0300

    all files added

---
+++ pythonProject1/main.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+import plotly.graph_objects as go
+from plotly.offline import plot
+import matplotlib.pyplot as plt
+import datetime
+from pycoingecko import CoinGeckoAPI
+from mplfinance.original_flavor import candlestick2_ohlc
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
